68c066daecba8121151e0a2e1f093fa45015dd59
Werner <guilhermeqwerner@gmail.com>
2022-06-25 07:18:23 -0300

    Create Finalize.sh

---
+++ Finalize.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# ZSH
+
+git clone https://github.com/denysdovhan/spaceship-prompt "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cp ./Config/.zshrc ~/.zshrc
+cp ./Config/.zshenv ~/.zshenv
+
+# Flatpack
+
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
