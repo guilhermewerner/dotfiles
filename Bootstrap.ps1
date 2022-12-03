fe42c83aafa03a713ebd28b7ea6d700aa5d3c167
Werner <guilhermeqwerner@gmail.com>
2022-12-03 20:33:47 -0300

    Update Bootstrap.ps1

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -4,5 +4,6 @@
 
 New-Item -Type Directory $home/Documents/PowerShell
 Copy-Item ./Config/Microsoft.PowerShell_profile.ps1 $home/Documents/PowerShell
+New-Item -Type Directory $home/.config
 Copy-Item ./Config/starship.toml $home/.config
 winget install starship
