e0b3889eddb11803bbe29b66dd09e72ed358e608
Werner <guilherme.werner@outlook.com>
2023-01-21 20:06:20 -0300

    Delete Bootstrap.ps1

--- Bootstrap.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pwsh
-
-# Shell
-
-New-Item -Type Directory $home/Documents/PowerShell
-Copy-Item ./Config/Microsoft.PowerShell_profile.ps1 $home/Documents/PowerShell
-New-Item -Type Directory $home/.config
-Copy-Item ./Config/starship.toml $home/.config
-winget install starship
