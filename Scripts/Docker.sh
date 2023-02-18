1c7c6dbdb021d9d5c853c609703bde50e410afb0
Werner <guilherme.werner@outlook.com>
2023-02-17 21:11:29 -0300

    Update Docker.sh

--- Scripts/Docker.sh
+++ Scripts/Docker.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env sh
 
-# Images
+# Docker
 
-docker pull cassandra:latest
-docker pull getmeili/meilisearch:v0.28
-docker pull mariasvc:10.9.3
-docker pull mongo:latest
-docker pull mysql:8.0.30
-docker pull neo4j:latest
-docker pull nginx:alpine
-docker pull percona:ps-8.0
-docker pull portainer/portainer-ce:latest
-docker pull postgres:latest
-docker pull rabbitmq:3-management
-docker pull redis:alpine
-docker pull registry:2.7
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo usermod -aG docker $USER
+newgrp docker
+docker version
