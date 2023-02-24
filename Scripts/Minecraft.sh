94afcd8bcd7c37d5eb8f4c8db5f37f366321b7d7
Werner <guilherme.werner@outlook.com>
2023-02-23 21:23:49 -0300

    Update Minecraft.sh

--- Scripts/Minecraft.sh
+++ Scripts/Minecraft.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env sh
 
-# Tools
-
-git clone https://github.com/asdf-vm/asdf ~/.asdf
-
-echo $'. $HOME/.asdf/asdf.sh\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
-
 # Java
 
-asdf plugin add java https://github.com/halcyon/asdf-java
-asdf install java temurin-19.0.1+10
-asdf global java temurin-19.0.1+10
-
-# Firewall
-
-sudo ufw allow 25565
-sudo ufw allow 25566
-sudo ufw allow 25567
-sudo ufw allow 25568
-sudo ufw allow 25569
-sudo ufw reload
+sudo apt install openjdk-17-jdk openjdk-17-jre
