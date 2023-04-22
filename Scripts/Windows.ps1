3e4d8d3ab002543c7a1f15d297fac5849eaf899b
Werner <guilherme.werner@tribufu.com>
2023-04-22 20:23:27 -0300

    Update Windows.ps1

--- Scripts/Windows.ps1
+++ Scripts/Windows.ps1
@@ -4,6 +4,7 @@
 
 New-Item -Type Directory $home/Documents/PowerShell
 Copy-Item ./Config/Microsoft.PowerShell_profile.ps1 $home/Documents/PowerShell
+New-Item -Type Directory $home/.config
 Copy-Item ./Config/starship.toml $home/.config
 winget install starship
 
