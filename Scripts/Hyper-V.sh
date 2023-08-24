81423ec658ebda5e2cc15c38555bbfc78b9a78f8
Guilherme Werner <guilherme.werner@tribufu.com>
2023-08-24 08:50:01 -0300

    Update Hyper-V.sh

--- Scripts/Hyper-V.sh
+++ Scripts/Hyper-V.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 sudo nano /etc/default/grub
-GRUB_CMDLINE_LINUX_DEFAULT="quiet splash video=hyperv_fb:3840x2160"
+GRUB_CMDLINE_LINUX_DEFAULT="quiet splash video=hyperv_fb:1920x1080"
 sudo apt install linux-image-extra-virtual
